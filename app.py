2d66f6d9e0363c892f4551579d5a01c6b73fa386
dave12jr <diopdave12@gmail.com>
2026-01-31 13:35:24 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Student
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Créer la base de données au démarrage si elle n'existe pas
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students=students)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        major = request.form['major']
+        
+        new_student = Student(first_name=first_name, last_name=last_name, email=email, major=major)
+        db.session.add(new_student)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_student(id):
+    student = Student.query.get_or_404(id)
+    if request.method == 'POST':
+        student.first_name = request.form['first_name']
+        student.last_name = request.form['last_name']
+        student.email = request.form['email']
+        student.major = request.form['major']
+        
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', student=student)
+
+@app.route('/delete/<int:id>')
+def delete_student(id):
+    student = Student.query.get_or_404(id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
