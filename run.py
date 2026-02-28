bbb0f650fbeab28ef81ca1ef6544d299d0728daa
dave12jr <diopdave12@gmail.com>
2026-02-28 12:39:03 +0000

    Update run.py

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from app.models import db
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(getenv('APP_PORT', '5006'))
+    port = int(getenv('APP_PORT', 5006))
     app.run(host='0.0.0.0', port=port)
